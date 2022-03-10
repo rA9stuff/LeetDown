cbe7b8454d2a52af741850844e536a04aeae324e
mini_exploit <61931266+Mini-Exploit@users.noreply.github.com>
2022-03-10 13:49:20 +0700

    Add files via upload

---
+++ LeetDown_M/bspatch.h
@@ -0,0 +1,19 @@
+//
+//  bspatch.h
+//  LeetDown
+//
+//  Created by macbookair on 3/10/22.
+//
+
+#ifndef bspatch_h
+#define bspatch_h
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+int doBSPatch(const char *oldfile, const char *newfile, const char *patchfile);
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* bspatch_h */
