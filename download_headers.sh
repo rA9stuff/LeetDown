45d451bb915b7b913f495727434ed54cc19d3891
rA9stuff <barisulascukur@gmail.com>
2023-11-25 13:54:01 +0300

    fix header paths

---
+++ download_headers.sh
@@ -0,0 +1,6 @@
+mkdir headers && cd headers
+git clone https://github.com/libimobiledevice/libimobiledevice.git
+git clone https://github.com/libimobiledevice/libirecovery.git
+git clone https://github.com/libimobiledevice/libusbmuxd.git
+git clone https://github.com/libimobiledevice/libplist.git
+git clone https://github.com/libimobiledevice/libimobiledevice-glue.git
